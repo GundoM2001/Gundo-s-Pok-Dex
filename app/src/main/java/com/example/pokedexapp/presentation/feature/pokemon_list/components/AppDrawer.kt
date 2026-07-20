99e780417ee0a539ba197aa7f3937d0cb53c6792
GundoM2001 <gundom@droppa.co.za>
2026-07-20 13:51:23 +0200

    AppDrawer.kt

--- app/src/main/java/com/example/pokedexapp/presentation/feature/pokemon_list/components/AppDrawer.kt
+++ app/src/main/java/com/example/pokedexapp/presentation/feature/pokemon_list/components/AppDrawer.kt
@@ -1,11 +1,20 @@
 package com.example.pokedexapp.presentation.feature.pokemon_list.components
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -16,17 +25,43 @@ fun AppDrawer(
     ModalDrawerSheet {
         Text(
             text = "Pokédex",
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(16.dp),
         )
+        HorizontalDivider(modifier = Modifier.padding(bottom = 8.dp))
         NavigationDrawerItem(
-            label = { Text("Home") },
+            label = { Text("Home", style = MaterialTheme.typography.labelLarge) },
             selected = false,
-            onClick = onHomeClick
+            icon = { Icon(imageVector = Icons.Default.Home, contentDescription = null) },
+            onClick = onHomeClick,
+            modifier = Modifier.padding(horizontal = 12.dp)
         )
         NavigationDrawerItem(
-            label = { Text("Favourites") },
+            label = { Text("Favourites", style = MaterialTheme.typography.labelLarge) },
             selected = false,
-            onClick = onFavoritesClick
+            icon = { Icon(imageVector = Icons.Default.Favorite, contentDescription = null) },
+            onClick = onFavoritesClick,
+            modifier = Modifier.padding(horizontal = 12.dp)
+        )
+
+        NavigationDrawerItem(
+            label = { Text("Settings", style = MaterialTheme.typography.labelLarge) },
+            selected = false,
+            icon = { Icon(imageVector = Icons.Default.Settings, contentDescription = null) },
+            onClick = onFavoritesClick,
+            modifier = Modifier.padding(horizontal = 12.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppDrawerPreview() {
+    MaterialTheme {
+        AppDrawer(
+            onHomeClick = {},
+            onFavoritesClick = {}
         )
     }
 }
--- app/src/main/java/com/example/pokedexapp/presentation/feature/pokemon_list/ui/PokemonListScreen.kt
+++ app/src/main/java/com/example/pokedexapp/presentation/feature/pokemon_list/ui/PokemonListScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.filled.Close
